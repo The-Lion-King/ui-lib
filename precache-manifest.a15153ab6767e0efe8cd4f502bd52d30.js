9478defebcc267f2528ee5d3066f25f879f1c9eb
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-05-10 16:14:49 +0000

    Deploy The-Lion-King/ui-lib to github.com/The-Lion-King/ui-lib.git:gh-pages

---
+++ precache-manifest.a15153ab6767e0efe8cd4f502bd52d30.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12cf083807085d36ccd330626e78f360",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.0a884e6b6e4539390c6c.bundle.js"
+  },
+  {
+    "url": "runtime~main.0a884e6b6e4539390c6c.bundle.js"
+  },
+  {
+    "revision": "ba3e05b4db4c341e0044",
+    "url": "static/css/main.00e1dd30.chunk.css"
+  },
+  {
+    "url": "vendors~main.0a884e6b6e4539390c6c.bundle.js"
+  },
+  {
+    "url": "vendors~main.0a884e6b6e4539390c6c.bundle.js.LICENSE.txt"
+  }
+]);
